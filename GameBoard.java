94bd9e90c1814f5a044c9d74dc591fe769d40cbd
Jeramy Dickerson <jrdicke@sandia.gov>
2026-09-09 07:19:19 -0600

    Adding an empty constructor that should throw an error, we can't use it

--- GameBoard.java
+++ GameBoard.java
@@ -1,3 +1,12 @@
+import cs251.project2.GomokuInterface.Square;
+
 public class GameBoard {
+    public GameBoard() {
+        //throw new UnsupportedOperationException("Must call GameBoard(cols, rows) instead");
+    }
     
+    public GameBoard(int columns, int rows){
+
+    }
+
 }
--- GameBoardTest.java
+++ GameBoardTest.java
@@ -1,15 +1,24 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import cs251.project2.Interface.Square;
 
 class GameBoardTest {
 
     @Test
-    void canInstantiateGameBoard() {
-        // Exercise the constructor
-        GameBoard board = new GameBoard();
-
-        // Verify it actually returned a non-null object
-        assertNotNull(board, "GameBoard should be instantiable");
+    void defaultConstructorFails() {
+        assertThrows(UnsupportedOperationException.class,
+            () -> new GameBoard(),
+            "No-arg GameBoard() should throw");
     }
+
+    @Test
+    void boardIsMadeOfSquareEnums() {
+       // GameBoard board = new GameBoard(3, 3);
+      //  assertInstanceOf(Square, board[1][1]);
+    }
+
+
+
+
 }
