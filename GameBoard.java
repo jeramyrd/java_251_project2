e8c752cf06f89c170c5469be9e7af0c7fcb2b6bc
Jeramy Dickerson <jrdicke@sandia.gov>
2026-09-09 07:24:14 -0600

    Pass test

--- GameBoard.java
+++ GameBoard.java
@@ -2,7 +2,7 @@ import cs251.project2.GomokuInterface.Square;
 
 public class GameBoard {
     public GameBoard() {
-        //throw new UnsupportedOperationException("Must call GameBoard(cols, rows) instead");
+        throw new UnsupportedOperationException("Must call GameBoard(cols, rows) instead");
     }
     
     public GameBoard(int columns, int rows){
