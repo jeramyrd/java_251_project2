5e8599a44ff0c7cd2e55b49d9cc248e857089f12
Jeramy Dickerson <jrdicke@sandia.gov>
2026-09-09 21:17:45 -0600

    Need these files

---
+++ Computer.java
@@ -0,0 +1,82 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    
+    private GameBoard gameBoard;
+    List<int[]> emptySpotsList;
+
+    Computer(GameBoard board){
+        gameBoard = board;
+    }
+    
+    //As a reminder the computer is always the CROSS.
+    public int[] bestMove(){
+        var testList = gameBoard.listEmptySpots();
+        double crossHighScore = 0;
+        int[] bestCrossSpot = {-1, -1};
+        double scale = 1.0 / (4.0 * gameBoard.getWinCount());
+        //System.out.println("Scale is: " + scale);
+        int[] directionColumn = {1, 0, 1, -1};
+        int[] directionRow = {0, 1, 1, 1};
+        int crossCount = 0, ringCount = 0;
+        double crossLocalScore = 0;
+        int tempCol = 0;
+        int tempRow = 0;
+        int offGridScaler = 1;
+
+        // loop through all empty spots
+        //System.out.println("STARTING THE SCORING SPREE!!!!!");
+        for (int[] spot: testList){            
+            //System.out.println("Empty Spot (" + spot[0] + "," + spot[1] +") ");
+            // Need to calculate 4 different directions
+            crossLocalScore = 0;
+            for (int directionIndex = 0; directionIndex < 4; ++directionIndex){
+                //System.out.println("Direction: " + directionIndex + " ---> 0 right 1 down 2 Dup 3 Ddown");
+                // Now we have userSelectedNumInLineForWin sets to calculate....
+                for (int setShift = gameBoard.getWinCount() - 1; setShift >= 0; --setShift ){
+                    //System.out.println("Setshift is " + setShift);
+                    tempCol = spot[0]-directionColumn[directionIndex]*setShift;
+                    tempRow = spot[1]-directionRow[directionIndex]*setShift;
+                    //System.out.println("SetShifted Start spot is (" + tempCol + "," + tempRow +") ");
+                    crossCount = 0;
+                    ringCount = 0;
+                    offGridScaler = 1;
+                    //Now finally, calculate the block. 
+                    for (int i = 0; i < gameBoard.getWinCount(); ++i){
+                        //System.out.println("Array Spot is currently (" + tempCol + "," + tempRow +") ");
+                        if (gameBoard.isSpotOnBoard(tempCol, tempRow)){
+                            switch (gameBoard.getSpot(tempCol, tempRow)) {
+                                case CROSS:
+                                    ++crossCount;
+                                    break;
+                                case RING:
+                                    ++ringCount;
+                                    break;
+                                case EMPTY:
+                                    break;
+                            }
+                        }
+                        else { offGridScaler = 0; }
+                        tempCol += directionColumn[directionIndex];
+                        tempRow += directionRow[directionIndex];
+                    }
+                    //System.out.println("Offgrid (zero is yes): " + offGridScaler);
+                    //System.out.println("crossCount = " + crossCount + " So math is:" + Math.pow(10, crossCount));
+                    //System.out.println("ringCount = " + ringCount + " So math is:" + Math.pow(10, ringCount));
+                    //finished the set - add to total
+                    //System.out.println("Cross score add would be: " + offGridScaler*scale*Math.pow(10, crossCount));
+                    //System.out.println("Ring score add would be: " + offGridScaler*scale*Math.pow(10, ringCount));
+                    if (ringCount == 0) { crossLocalScore += offGridScaler*scale*Math.pow(10, crossCount);}
+                    //System.out.println("Ring Local score: " + ringLocalScore + " Cross Local Score: " + crossLocalScore);
+                }
+            }
+            if (crossLocalScore > crossHighScore) {
+                crossHighScore = crossLocalScore;
+                bestCrossSpot = spot;
+            }
+            //System.out.println("Cross High score: " + crossHighScore + " Cross best spot (" + bestCrossSpot[0] + "," + bestCrossSpot[1] + ")");
+        }
+        return bestCrossSpot;
+    }
+}
